406bf15aff5320ff4cc9260e58fa4440ac375772
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-23 16:52:41 -0400

    add fetuare queue fila

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,7 +5,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -143,18 +144,7 @@ class AppointmentController {
 
     await appointment.save();
 
-    await mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às ' HH:mm'h' ", {
-          locale: pt,
-        }),
-      },
-    });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
