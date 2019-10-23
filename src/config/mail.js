f2971314800de6484448cf64c518bb4e176e3e28
douglasgomes98 <douglasgomes.rr@gmail.com>
2019-10-23 14:38:56 -0400

    add fetuare email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'ecfecc953c22fd',
+    pass: '25b75d35f9ce44',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
